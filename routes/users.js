9a62f5d34bfea81852ae186ef455e38734d4d665
gabe <gabrielz022604@gmail.com>
2026-03-30 23:50:51 -0400

    end of day commit

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,15 @@ const router = express.Router();
 
 router.get('/', (req,res)=>{
     res.send('User List')
-})
+});
 
 router.get('/new', (req,res)=>{
     res.send('User New Form')
-})
+});
+
+router.get('/:id', (req,res) =>{
+    res.send(`Getting user Data: ${req.params.id}`)
+});
+
 
 module.exports = router;
