33a8ff9963166157c4966ede42ec2ade07dc733e
g.zambrana <gabrielz022604@gmail.com>
2026-03-31 13:40:13 -0400

    adding form ejs

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-    res.send('User List')
+router.route('/').get((req,res)=>{
+    res.send('User List');
+}).post((req,res) =>{
+    const firstName = req.body.firstName;
+    const isValid = firstName !=="";
+    if(isValid){
+        console.log(`Adding User: ${firstName}`);
+        users.push({firstName});
+        res.render('users/list',{users});
+    } else{
+        console.log("Error adding user");
+        res.render("users/new", {firstName:firstName})
+    }
+});
+router.get('/list', (req,res)=>{
+    res.render('users/list', {users});
 });
 
 router.get('/new', (req,res)=>{
-    res.send('User New Form')
+    res.render('users/new', {firstName:"test"})
 });
 
-router.get('/:id', (req,res) =>{
-    res.send(`Getting user Data: ${req.params.id}`)
+
+router.route('/:id').get((req,res) =>{
+    console.log(req.user);
+    console.log("Getting User Data!");
+    res.send(`Getting user Data: ${req.user['name']}`);
+}).delete((req,res)=>{
+    res.send(`Deleting User Data for id: ${req.params.id}`);
+}).put((req,res) =>{
+    res.send(`Updating User Data for id: ${req.params.id}`);
 });
+
+const users = [{firstName:"gabe"}, {firstName:"jess"}];
+router.param("id", (req,res,next,id)=>{
+    req.user = users[id];
+    next();
+});
+
 
 
 module.exports = router;
