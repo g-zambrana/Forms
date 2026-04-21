b4e85944ee0075657a31f175d59f546d9dcc59a2
gabe <gabrielz022604@gmail.com>
2026-04-20 20:17:37 -0400

    made function for shuffle and get random part

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const {readFile} = require('fs').promises;
 
+router.get('/', (req, res) => {
+    
+});
 
+let getWords = async () => {
+    let randomPart = getRandomPart();
+}
 
+let getRandomPart = ()=>{
+    let parts = ['noun', 'verb', 'adjective'];
+    let randomIndex = Math.random() * parts.length;
+    let randomPart = parts[randomIndex];
+    return randomPart;
+}
 
-
-
+let shuffle = (array) => {
+    for(let i=0; i<array.length; i--)
+    {
+        let randomNumber = Math.floor(Math.random()*(i+1));
+        [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
+    }
+}
 
 module.exports = router;
