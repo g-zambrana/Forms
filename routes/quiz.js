51059ac15aa08a8917f1200a7903e3675835004c
gabe <gabrielz022604@gmail.com>
2026-04-20 20:50:04 -0400

    generated chosen words array and sent to ejs

--- routes/quiz.js
+++ routes/quiz.js
@@ -2,27 +2,46 @@ const express = require('express');
 const router = express.Router();
 const {readFile} = require('fs').promises;
 
-router.get('/', (req, res) => {
-    
+router.get('/', async (req, res) => {
+    let chosenWords = await getWords();
+    console.log("Chosen Words:", chosenWords);
 });
 
 let getWords = async () => {
-    let randomPart = getRandomPart();
+    let allWords = await readFile ('resources/allwords.txt', 'utf-8');
+    let wordArray = allWords.split('\n');
+    console.log(wordArray);
+    shuffle(wordArray);
+    let choices = [];
+    while(choices.length< 5){
+        let line = wordArray.pop();
+        //let [word, part, def]= line.split('\t');
+        let tokens = line.split('\t');
+        let word = tokens[0];
+        let part = tokens[1];
+        let def = tokens[2];
+        if (part === randomPart){
+            choices.push(line);
+        }
+    }
+    return choices;
+
 }
 
 let getRandomPart = ()=>{
     let parts = ['noun', 'verb', 'adjective'];
-    let randomIndex = Math.random() * parts.length;
+    let randomIndex = Math.floor(Math.random() * parts.length);
     let randomPart = parts[randomIndex];
     return randomPart;
 }
 
 let shuffle = (array) => {
-    for(let i=0; i<array.length; i--)
+    for(let i = array.length - 1; i > 0; i--)
     {
         let randomNumber = Math.floor(Math.random()*(i+1));
         [array[i], array[randomNumber]] = [array[randomNumber], array[i]];
     }
+    console.log("array shuffled");
 }
 
 module.exports = router;
